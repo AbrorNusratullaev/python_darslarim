d916145ef2a30c5f81bdb94bc72f299f6a8cd148
Abrorjon Nusratullaev <110447039+AbrorNusratullaev@users.noreply.github.com>
2023-07-25 16:02:10 +0500

    yangi fayllar qo'shildi
    dars namuna

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 06:13:39 2023
+
+@author: user
+"""
+
+yosh = int(input('yoshingiz nechida? '))
+if yosh<=4:
+    narx = 0
+elif yosh<=12:
+    narx = 5000
+elif yosh<=18:
+    narx = 8000
+else: narx = 10000
+print(f"Sizga kirish {narx} so'm")
